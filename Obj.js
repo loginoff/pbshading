9c4e9294100af35ea5958205cc5bb5e8f6629bb1
Martin Loginov <martin.loginov@ut.ee>
2013-12-09 01:38:03 +0200

    Started writing my own OBJ loader for GLOW

--- Obj.js
+++ Obj.js
@@ -2,12 +2,54 @@ function readOBJ ( str_obj ) {
 	var str_obj = str_obj.replace( "\r", '' );
 	var arr_rows = str_obj.split( "\n" );
 	var length = arr_rows.length;
-	
+
+    var objects = {};
 	var vertices = [];
 	var texCoords = [];
 	var faces = [];
 	var normals = [];
 
+    //lets do a pass and enumerate all the objects and stuff
+    var cur_obj;
+    for (i = 0; i < length; i++){
+        var row = arr_rows[i];
+        var str_val = row.substr(0,2);
+        if(str_val == 'o '){
+            cur_obj = new Object();
+            cur_obj.numVertices = 0;
+            cur_obj.numNormals = 0;
+            cur_obj.numUVs = 0;
+            cur_obj.name = row.substr(2,row.length);
+            objects[cur_obj.name] = cur_obj;
+        }
+        if(cur_obj && str_val == 'v '){
+            cur_obj.numVertices++;
+        }
+        if(cur_obj && str_val == 'vn'){
+            cur_obj.numNormals++;
+        }
+        if(cur_obj && str_val == 'vt'){
+            cur_obj.numUVs++;
+        }
+    }
+    for(obj in objects){
+        cur_obj = objects[obj];
+        cur_obj.vertices = new Float32Array(cur_obj.numVertices*3);
+        if(cur_obj.numNormals){
+            cur_obj.normals = new Float32Array(cur_obj.numNormals*3);
+        }
+        if(cur_obj.numUVs){
+            cur_obj.UVs = new Float32Array(cur_obj.numUVs*2);
+        }
+    }
+
+    cur_obj = undefined;
+    var ivert = 0;
+    var iuv = 0;
+    var inormal = 0;
+    for (i = 0; i < length; i++) {
+
+    }
 	
 	for ( i = 0; i < length; i++ ) {
 		var str_row = arr_rows[ i ];
